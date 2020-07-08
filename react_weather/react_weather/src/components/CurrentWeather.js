b33b3952ffcc81416cfd1471c52bbe4b02b4fbad
unknown <piwowarczykpatrick@gmail.com>
2020-07-08 13:42:47 -0500

    Added fontawesome library.

--- react_weather/react_weather/src/components/CurrentWeather.js
+++ react_weather/react_weather/src/components/CurrentWeather.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+
 
 export class CurrentWeather extends Component {
     render() {
+        const sunny = <i className="fas fa-sun" style={{color: "#000000" }}></i>;
         const currentWeatherInfo = this.props.weather;
         console.log(this.props.weather.weatherInfo[0].id);
         return (
             <div>
                 <h3>Current Temperature: {this.props.weather.weatherInfo[0].current}</h3>
+                <FontAwesomeIcon icon={faHome} />
             </div>
         )
     }
